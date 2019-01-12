bc239105bf98b449212b687631ab617b6c34e10f
Roman Lazan <roman.lazan@check24.de>
2019-01-12 20:25:47 +0100

    NOTEAPP-0001: Project setup

---
+++ Notes/Notes/NotesViewCotroller.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Roman Lazan on 11.01.19.
+//  Copyright © 2019 Roman Lazan. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewCotroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
